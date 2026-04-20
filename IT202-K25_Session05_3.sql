45eabc53d40781de42e9afc6367e49e3f617014b
Nguyễn Hoàng Anh <143367993+dfrgtyhujio@users.noreply.github.com>
2026-04-20 22:23:14 +0700

    Add files via upload

--- IT202-K25_Session05_3.sql
+++ IT202-K25_Session05_3.sql
@@ -1,13 +1,28 @@
 -- Dữ liệu đầu vào:
 -- restaurant_location: tọa độ quán ăn
--- min_trust_score: Điểm tín nhiệm tối thiểu
 -- drivers: bảng dữ liệu tài xế
+
+use db_demo;
+
+create table Drivers (
+    driver_id int,
+    status varchar(20),
+    trust_score int,
+    distance_km decimal(5,2)
+);
+
+insert into Drivers values
+	(1, 'AVAILABLE', 90, 2.5),
+	(2, 'BUSY', 85, 1.0),
+	(3, 'AVAILABLE', 75, 3.0),
+	(4, 'AVAILABLE', 88, 1.5),
+	(5, 'AVAILABLE', 92, 1.5);
 
 SELECT driver_id, status, trust_score, distance_km
 FROM Drivers
 WHERE 
 	status = 'AVAILABLE' 
-    AND trust_score >= min_trust_score
+    AND trust_score >= 80
 ORDER BY 
     distance_km ASC,
     trust_score DESC;
